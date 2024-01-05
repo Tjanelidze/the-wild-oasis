64ab516a90d7aed2ea390297cdaad1512a27bd73
Tedo <tedojanelidze7@gmail.com>
2024-01-05 17:26:21 +0400

    created reusable row and button

--- src/App.jsx
+++ src/App.jsx
@@ -14,23 +14,31 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">The Wild Oasis</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
 
-          <div>
-            <Heading as="h2">Check in and out</Heading>
-            <Button onClick={() => alert('Check in')}>Check in</Button>
-            <Button onClick={() => alert('Check out')}>Check out</Button>
-          </div>
-        </Row>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert('Check in')}>Check in</Button>
+              <Button
+                variation="secondary"
+                sieze="small"
+                onClick={() => alert('Check out')}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Heading as="h3">Form</Heading>
+          <Heading as="h3">Form</Heading>
 
-        <Row type="vertical">
-          <form>
-            <Input type="number" placeholder="Number of guests" />
-            <Input type="number" placeholder="Number of guests" />
-          </form>
+          <Row>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
         </Row>
       </StyledApp>
     </>
