da9ea1898e34b795f2af5d85b0c8e047656f914e
Tedo <tedojanelidze7@gmail.com>
2024-01-11 18:16:04 +0400

    added sorting functionality for asceding and descending

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -12,6 +12,7 @@ export default function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  // 1) FILTER
   const filterValue = searchParams.get('discount') || 'all';
 
   let filteredCabins;
@@ -21,9 +22,17 @@ export default function CabinTable() {
   if (filterValue === 'with-discount')
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
+  // 2) SORT
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
-      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+      <Table $columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
         <Table.Header>
           <div></div>
           <div>Cabin</div>
@@ -34,7 +43,7 @@ export default function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
